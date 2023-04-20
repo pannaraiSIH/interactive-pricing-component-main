1d0bdb6ef3c747067a86ca719a82834edf109081
pannaraiSIH <pannarai.sih@student.mahidol.edu>
2023-04-21 00:17:56 +0700

    modified input tag

--- js/script.js
+++ js/script.js
@@ -19,23 +19,23 @@ btnPayment.addEventListener("click", () => {
 });
 
 sliderBar.addEventListener("change", () => {
-  if (sliderBar.value <= 1) {
+  if ((sliderBar.value = 0)) {
     pageviews.innerHTML = "10K";
     pricing.innerHTML = "$8/month";
     discountPricing.innerHTML = "$72/year";
-  } else if (sliderBar.value <= 3) {
+  } else if ((sliderBar.value = 1)) {
     pageviews.innerHTML = "50K";
     pricing.innerHTML = "$12/month";
     discountPricing.innerHTML = "$108/year";
-  } else if (sliderBar.value <= 5) {
+  } else if ((sliderBar.value = 2)) {
     pageviews.innerHTML = "100K";
     pricing.innerHTML = "$16/month";
     discountPricing.innerHTML = "$144/year";
-  } else if (sliderBar.value <= 7) {
+  } else if ((sliderBar.value = 3)) {
     pageviews.innerHTML = "500K";
     pricing.innerHTML = "$24/month";
     discountPricing.innerHTML = "$216/year";
-  } else if (sliderBar.value <= 9) {
+  } else if ((sliderBar.value = 4)) {
     pageviews.innerHTML = "1M";
     pricing.innerHTML = "$36/month";
     discountPricing.innerHTML = "$648/year";
@@ -44,6 +44,6 @@ sliderBar.addEventListener("change", () => {
 
 sliderBar.addEventListener("input", () => {
   sliderBar.style.background = `linear-gradient(to right, var(--full-slider) ${
-    sliderBar.value * 10
-  }%, var(--empty-slider) ${sliderBar.value * 10}% 100%)`;
+    sliderBar.value * 25
+  }%, var(--empty-slider) ${sliderBar.value * 25}% 100%)`;
 });
