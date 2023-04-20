c8e8afb64175cf5a103748d127afa4fb8a93d0a9
pannaraiSIH <pannarai.sih@student.mahidol.edu>
2023-04-21 00:06:57 +0700

    modified yearly price

--- js/script.js
+++ js/script.js
@@ -21,24 +21,24 @@ btnPayment.addEventListener("click", () => {
 sliderBar.addEventListener("change", () => {
   if (sliderBar.value <= 1) {
     pageviews.innerHTML = "10K";
-    pricing.innerHTML = "$8";
-    discountPricing.innerHTML = "$72";
+    pricing.innerHTML = "$8/month";
+    discountPricing.innerHTML = "$72/year";
   } else if (sliderBar.value <= 3) {
     pageviews.innerHTML = "50K";
-    pricing.innerHTML = "$12";
-    discountPricing.innerHTML = "$108";
+    pricing.innerHTML = "$12/month";
+    discountPricing.innerHTML = "$108/year";
   } else if (sliderBar.value <= 5) {
     pageviews.innerHTML = "100K";
-    pricing.innerHTML = "$16";
-    discountPricing.innerHTML = "$144";
+    pricing.innerHTML = "$16/month";
+    discountPricing.innerHTML = "$144/year";
   } else if (sliderBar.value <= 7) {
     pageviews.innerHTML = "500K";
-    pricing.innerHTML = "$24";
-    discountPricing.innerHTML = "$216";
+    pricing.innerHTML = "$24/month";
+    discountPricing.innerHTML = "$216/year";
   } else if (sliderBar.value <= 9) {
     pageviews.innerHTML = "1M";
-    pricing.innerHTML = "$36";
-    discountPricing.innerHTML = "$648";
+    pricing.innerHTML = "$36/month";
+    discountPricing.innerHTML = "$648/year";
   }
 });
 
